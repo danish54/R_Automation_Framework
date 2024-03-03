1ac6b0a27e30e236b29e20c65aacda2e9d825ad0
Danish Ali <ali.danish54@gmail.com>
2024-03-03 17:15:13 +0530

    listners

---
+++ src/main/java/resources/Listeners.java
@@ -0,0 +1,48 @@
+package resources;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+public class Listeners implements ITestListener {
+
+	ExtentTest test;
+	ExtentReports extent = ExtendReportsNG.getExtentReportsObject();
+	ThreadLocal<ExtentTest> extentTest = new ThreadLocal<ExtentTest>();
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		test = extent.createTest(result.getMethod().getMethodName());
+		extentTest.set(test);
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		extentTest.get().log(Status.PASS, "Test Passed");
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		extentTest.get().log(Status.FAIL, "Test Failed");
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		extentTest.get().log(Status.SKIP, "Test Skipped");
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		extent.flush();
+	}
+
+}
